2d4d29b4cb2fbe3c6f5fc05d931cccb2f3ab91c5
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-22 22:33:20 +0700

    Create Handle deeply nested data.sql

---
+++ 4.Importing JSON Data and Working with APIs/Handle deeply nested data.sql
@@ -0,0 +1,30 @@
+# Flatten businesses records and set underscore separators
+flat_cafes = json_normalize(data["businesses"],
+                  sep="_")
+
+# View the data
+print(flat_cafes.head())
+
+
+# Specify record path to get categories data
+flat_cafes = json_normalize(data["businesses"],
+                            sep="_",
+                    		record_path="categories")
+
+# View the data
+print(flat_cafes.head())
+
+
+# Load other business attributes and set meta prefix
+flat_cafes = json_normalize(data["businesses"],
+                            sep="_",
+                    		    record_path="categories",
+                    		    meta=["name",
+								                  "alias", 
+                                  "rating",  
+                          		    ["coordinates", "latitude"], 
+                          		    ["coordinates", "longitude"]],
+                    	      meta_prefix="biz_")
+
+# View the data
+print(flat_cafes.head())
