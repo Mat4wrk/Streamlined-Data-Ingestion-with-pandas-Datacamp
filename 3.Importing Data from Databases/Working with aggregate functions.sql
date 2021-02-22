7f669acac52d7b3baef5788efb35467493dcbb5e
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-22 22:09:41 +0700

    Create Working with aggregate functions.sql

---
+++ 3.Importing Data from Databases/Working with aggregate functions.sql
@@ -0,0 +1,45 @@
+# Create a query to get month and max tmax by month
+query = """
+SELECT month, 
+       MAX(tmax)
+  FROM weather 
+  GROUP BY month;"""
+
+# Get data frame of monthly weather stats
+weather_by_month = pd.read_sql(query, engine)
+
+# View weather stats by month
+print(weather_by_month)
+
+
+# Create a query to get month, max tmax, and min tmin by month
+query = """
+SELECT month, 
+	   MAX(tmax), 
+        MIN(tmin)
+  FROM weather 
+ GROUP BY month;
+"""
+
+# Get data frame of monthly weather stats
+weather_by_month = pd.read_sql(query, engine)
+
+# View weather stats by month
+print(weather_by_month)
+
+
+# Create query to get temperature and precipitation by month
+query = """
+SELECT month, 
+        MAX(tmax), 
+        MIN(tmin),
+        SUM(prcp)
+  FROM weather 
+ GROUP BY month;
+"""
+
+# Get data frame of monthly weather stats
+weather_by_month = pd.read_sql(query, engine)
+
+# View weather stats by month
+print(weather_by_month)
