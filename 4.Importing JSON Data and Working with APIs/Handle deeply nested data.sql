0813a94fd80acbe5b86cfab7b38be8ff8b150997
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-22 22:34:15 +0700

    Update Handle deeply nested data.sql

--- 4.Importing JSON Data and Working with APIs/Handle deeply nested data.sql
+++ 4.Importing JSON Data and Working with APIs/Handle deeply nested data.sql
@@ -18,13 +18,13 @@ print(flat_cafes.head())
 # Load other business attributes and set meta prefix
 flat_cafes = json_normalize(data["businesses"],
                             sep="_",
-                    		    record_path="categories",
-                    		    meta=["name",
-								                  "alias", 
+                    	    record_path="categories",
+                    	    meta=["name",
+				  "alias", 
                                   "rating",  
-                          		    ["coordinates", "latitude"], 
-                          		    ["coordinates", "longitude"]],
-                    	      meta_prefix="biz_")
+                              	  ["coordinates", "latitude"], 
+                          	  ["coordinates", "longitude"]],
+                    	    meta_prefix="biz_")
 
 # View the data
 print(flat_cafes.head())
