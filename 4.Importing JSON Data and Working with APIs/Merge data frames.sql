a65310f77ba740cb4f196823ec524d215af994f5
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-23 11:26:50 +0700

    Update Merge data frames.sql

--- 4.Importing JSON Data and Working with APIs/Merge data frames.sql
+++ 4.Importing JSON Data and Working with APIs/Merge data frames.sql
@@ -1,6 +1,6 @@
 # Merge crosswalk into cafes on their zip code fields
 cafes_with_pumas = cafes.merge(crosswalk, 
-                   			   left_on="location_zip_code", 
+                   			       left_on="location_zip_code", 
                                right_on="zipcode")
 
 # Merge pop_data into cafes_with_pumas on puma field
